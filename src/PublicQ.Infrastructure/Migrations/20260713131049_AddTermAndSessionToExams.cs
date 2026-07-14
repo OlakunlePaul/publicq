e24c92e26ce2a7a0d406c9cd1c570b5c33de963a
Paul Olagboye <olawaleolalekan33@gmail.com>
2026-07-14 01:09:15 +0100

    Explicitly set uuid type in EF migration for Npgsql compatibility

--- src/PublicQ.Infrastructure/Migrations/20260713131049_AddTermAndSessionToExams.cs
+++ src/PublicQ.Infrastructure/Migrations/20260713131049_AddTermAndSessionToExams.cs
@@ -14,21 +14,25 @@ namespace PublicQ.Infrastructure.Migrations
             migrationBuilder.AddColumn<Guid>(
                 name: "SessionId",
                 table: "Assignments",
+                type: "uuid",
                 nullable: true);
 
             migrationBuilder.AddColumn<Guid>(
                 name: "TermId",
                 table: "Assignments",
+                type: "uuid",
                 nullable: true);
 
             migrationBuilder.AddColumn<Guid>(
                 name: "SessionId",
                 table: "AssessmentModules",
+                type: "uuid",
                 nullable: true);
 
             migrationBuilder.AddColumn<Guid>(
                 name: "TermId",
                 table: "AssessmentModules",
+                type: "uuid",
                 nullable: true);
 
             migrationBuilder.CreateIndex(
