c66eeb2351c99ca9409ee5afeed9f098fb222081
Bot <olawaleolalekan33@gmail.com>
2026-08-16 19:03:59 +0100

    fix: Drop default constraint before altering column type

--- src/PublicQ.Infrastructure/Migrations/20260816175527_FixCumulativeTermScoresTypes.cs
+++ src/PublicQ.Infrastructure/Migrations/20260816175527_FixCumulativeTermScoresTypes.cs
@@ -10,7 +10,9 @@ namespace PublicQ.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("ALTER TABLE \"Terms\" ALTER COLUMN \"IsCumulativeTerm\" DROP DEFAULT;");
             migrationBuilder.Sql("ALTER TABLE \"Terms\" ALTER COLUMN \"IsCumulativeTerm\" TYPE boolean USING CASE WHEN \"IsCumulativeTerm\" = 0 THEN false ELSE true END;");
+            migrationBuilder.Sql("ALTER TABLE \"Terms\" ALTER COLUMN \"IsCumulativeTerm\" SET DEFAULT false;");
             migrationBuilder.Sql("ALTER TABLE \"SubjectScores\" ALTER COLUMN \"CumulativeAverage\" TYPE numeric USING \"CumulativeAverage\"::numeric;");
             migrationBuilder.Sql("ALTER TABLE \"SubjectScores\" ALTER COLUMN \"FirstTermScore\" TYPE numeric USING \"FirstTermScore\"::numeric;");
             migrationBuilder.Sql("ALTER TABLE \"SubjectScores\" ALTER COLUMN \"SecondTermScore\" TYPE numeric USING \"SecondTermScore\"::numeric;");
